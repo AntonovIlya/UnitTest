849bff5a59e21d189d1f5b00bd4eafae949cee24
AntonovIlya <ilyha.199@mail.ru>
2022-12-01 20:43:00 +0300

    edit concurrentCollections package

--- src/main/java/multithreading/concurrentCollections/Specialist.java
+++ src/main/java/multithreading/concurrentCollections/Specialist.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Specialist implements Runnable {
 
-    private static final int CALL_PROCESSING = 4000 + (new Random().nextInt(2000) - 1000);
+    private int callProcessing = 4000 + (new Random().nextInt(2000) - 1000);
 
     private final CallsQueue callsQueue;
 
@@ -18,7 +18,7 @@ public class Specialist implements Runnable {
             try {
                 String call = callsQueue.getBlockingQueue().poll();
                 System.out.println(Thread.currentThread().getName() + ": " + call + " processed!");
-                Thread.sleep(CALL_PROCESSING);
+                Thread.sleep(callProcessing);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
