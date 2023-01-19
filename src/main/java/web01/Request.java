66c2f6eca6705be9dcf865c8312bf685f50ca004
AntonovIlya <ilyha.199@mail.ru>
2023-01-19 19:25:52 +0300

    form handler added

--- src/main/java/web01/Request.java
+++ src/main/java/web01/Request.java
@@ -1,7 +1,12 @@
 package web01;
 
+import org.apache.http.client.utils.URIBuilder;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,22 +16,25 @@ public class Request {
 
     private String requestMethod;
     private String requestedURL;
+    private URIBuilder builder;
     private String protocolVersion;
     private final Map<String, String> bodyHeaders = new HashMap<>();
     private final StringBuilder messageBody = new StringBuilder();
+    private final Map<String, List<String>> postParams = new HashMap<>();
     private final BufferedReader data;
     private final List<Integer> listOfCodes = new ArrayList<>();
 
-    public Request(BufferedReader data) throws IOException {
+    public Request(BufferedReader data) throws IOException, URISyntaxException {
         this.data = data;
         parseRequest();
     }
 
-    public void parseRequest() throws IOException {
+    public void parseRequest() throws IOException, URISyntaxException {
         StringBuilder sb = new StringBuilder();
         String[] requestLine = data.readLine().split(" ");
         requestMethod = requestLine[0];
         requestedURL = requestLine[1];
+        builder = new URIBuilder(requestedURL);
         protocolVersion = requestLine[2];
         while (data.ready()) {
             int symbol = data.read();
@@ -70,5 +78,31 @@ public class Request {
 
     public String getMessageBody() {
         return messageBody.toString();
+    }
+
+    public String getPath() {
+        return builder.getPath();
+    }
+
+    public List<String> getPostParam(String name) {
+        return postParams.get(name);
+    }
+
+    public Map<String, List<String>> getPostParams() {
+        if (!(bodyHeaders.containsKey("Content-Type") && bodyHeaders.get("Content-Type").equals("application/x-www-form-urlencoded"))) return postParams;
+        String[] pairs = messageBody.toString().split("&");
+        for (String s : pairs) {
+            String[] nameValue = s.split("=");
+            String value = URLDecoder.decode(nameValue[1], StandardCharsets.UTF_8);
+            String name = nameValue[0];
+            if (postParams.containsKey(name)) {
+                postParams.get(name).add(value);
+                continue;
+            }
+            List<String> list = new ArrayList<>();
+            list.add(value);
+            postParams.put(name, list);
+        }
+        return postParams;
     }
 }
